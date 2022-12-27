6bc94a4796a992fce8de6502bf17533fb0d6865d
neojinxd <neojinxd@gmail.com>
2022-12-26 20:28:56 -0500

    added stb as external lib for Phoinix and implementation

--- Phoinix/src/Graphics/Image.cpp
+++ Phoinix/src/Graphics/Image.cpp
@@ -1,5 +1,10 @@
 #include "Image.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image.h>
+#include <stb_image_write.h>
+
 namespace Phoinix
 {
   Image* (*Image::CreateFunc)(uint32_t width, uint32_t height, Format format, void* data) = nullptr;
