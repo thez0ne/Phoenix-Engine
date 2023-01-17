b4d4ea505715a56b090b30eb2b9314b758c7ac2a
neoJINXD <neojinxd@gmail.com>
2023-01-17 08:48:55 -0500

    rendering single hard coded sphere

--- Raytracer/src/RayRenderer.cpp
+++ Raytracer/src/RayRenderer.cpp
@@ -21,6 +21,18 @@ namespace Raytracing
 
   void RayRenderer::Render()
   {
+    for (size_t y = 0; y < m_ViewportHeight; y++)
+    {
+      for (size_t x = 0; x < m_ViewportWidth; x++)
+      {
+        glm::vec2 remappedCoords = {(float)x / (float)m_FinalImage->GetWidth(),
+                                    (float)y / (float)m_FinalImage->GetHeight()};
+        remappedCoords = remappedCoords * 2.0f - 1.0f;
+        auto colour = PixelColour(remappedCoords);
+        m_ImageData[x + y * m_ViewportWidth] = Phoinix::Image::VecToRgba(colour);
+      }
+    }
+
     m_FinalImage->SetData(m_ImageData);
   }
 
@@ -34,11 +46,6 @@ namespace Raytracing
 
     delete[] m_ImageData;
     m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
-
-    for (size_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
-    {
-      m_ImageData[i] = Phoinix::Image::VecToRgba(m_BackgroundColour);
-    }
   }
 
   void RayRenderer::SaveImage(const std::string& fileName)
@@ -58,4 +65,24 @@ namespace Raytracing
   {
     ImGui::ColorEdit4("Background Colour", (float*)&m_BackgroundColour);
   }
+
+  glm::vec4 RayRenderer::PixelColour(glm::vec2 coords)
+  {
+    // TODO currently doesnt handle aspect ratio
+    Ray ray;
+    ray.origin = m_CameraCenter;
+    ray.dir = glm::normalize(glm::vec3(coords.x, coords.y, -1.f));
+
+    auto a = glm::dot(ray.dir, ray.dir);
+    auto b = 2.0 * dot(ray.origin - m_SpherePos, ray.dir);
+    auto c = dot(ray.origin - m_SpherePos, ray.origin - m_SpherePos) - m_Radius * m_Radius;
+
+    auto discriminant = b * b - 4.0f * a * c;
+    if (discriminant >= 0)
+      return m_SphereColour;
+
+    return m_BackgroundColour;
+  }
+
+  void RayRenderer::TraceRay(const Ray& ray) {}
 }
--- Raytracer/src/RayRenderer.h
+++ Raytracer/src/RayRenderer.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <Phoinix/Image.h>
-#include <glm/vec4.hpp>
+#include <glm/glm.hpp>
+
+#include "Ray.h"
 
 #include <string>
 
@@ -24,6 +26,17 @@ namespace Raytracing
     uint32_t* m_ImageData;
     uint32_t m_ViewportWidth, m_ViewportHeight;
 
-    glm::vec4 m_BackgroundColour = glm::vec4(1.0f, .0f, .0f, 1.0f);
+    glm::vec4 m_BackgroundColour = glm::vec4(.3f, .2f, .5f, 1.0f);
+
+    glm::vec4 PixelColour(glm::vec2 coords);
+    void TraceRay(const Ray& ray);
+
+    // Camera stuff
+    glm::vec3 m_CameraCenter = glm::vec3(0.f, 0.f, 2.f);
+
+    // Geometry stuff
+    float m_Radius = 0.5f;
+    glm::vec3 m_SpherePos = glm::vec3(.0f, 0.f, -1.f);
+    glm::vec4 m_SphereColour = glm::vec4(1.f, 0.f, 0.f, 1.0f);
   };
 }
