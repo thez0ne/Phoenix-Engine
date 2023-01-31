ff1d12b58595c2776dd3ed45b683d350c8a6c99d
neoJINXD <neojinxd@gmail.com>
2023-01-30 20:14:13 -0500

    made initial distance in hit info infinity

--- Raytracer/src/Objects/HitInformation.h
+++ Raytracer/src/Objects/HitInformation.h
@@ -12,6 +12,6 @@ namespace Raytracing
     glm::vec4 colour;
 
     float t;
-    float hitDistance = 999999999; // TODO infinity?
+    float hitDistance = std::numeric_limits<double>::infinity();
   };
 }
